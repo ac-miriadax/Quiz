cedd9902c75e8b5b5926ecb2eec069d2c9ddc037
ac-miriadax <notengo@email.com>
2015-08-03 23:55:40 +0200

    Módulo 8: Totalmente terminado, incluido ejercicio P2P, a falta de pequeños detalles.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,6 @@
 var models = require('../models/models.js');
 
+// Autoload
 exports.load = function(req, res, next, quizId){
   models.Quiz.findById(quizId).then(
     function(quiz){
@@ -14,24 +15,85 @@ exports.load = function(req, res, next, quizId){
   ).catch(function(error){next(error);});
 };
 
-exports.index = function(req, res){
+// Carga el listado de preguntas disponibles
+exports.index = function(req, res, next){
   models.Quiz.findAll().then(function(quizes){
-    res.render('quizes/index', {quizes: quizes});
-  }).catch(function(error){next(error);})
+    res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+  }).catch(function(error){next(error)});
 };
 
+// Carga el contenido de una pregunta
 exports.show = function(req, res){
-  res.render('quizes/show', {quiz: req.quiz});
+  res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
+// Comprueba si la respuesta es correcta o no
 exports.answer = function(req, res){
   var resultado = 'Incorrecto';
   if(req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()){
       resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
+// Carga el formulario de pregunta nueva
+exports.new = function(req, res){
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta", categoria: "Categoria"}
+  );
+
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// Crea la pregunta nueva
+exports.create = function(req, res){
+  var quiz = models.Quiz.build(req.body.quiz);
+  quiz.validate().then(
+    function(err){
+      if(err){
+        res.render('/quizes/new', {quiz: quiz, errors: err.errors});
+      }
+
+      else{
+        quiz.save({fields:["pregunta", "respuesta", "categoria"]}).then(function(){res.redirect('/quizes')})
+      }
+    }
+  );
+};
+
+// Editamos una pregunta existente
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then(
+    function(err){
+      if(err){
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      }
+
+      else{
+        req.quiz.save({fields:["pregunta", "respuesta", "categoria"]}).then(function(){res.redirect('/quizes')})
+      }
+    }
+  );
+};
+
+// Editar una pregunta
+exports.edit = function(req, res){
+  var quiz = req.quiz;
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// Elimina una pregunta
+exports.destroy = function(req, res){
+  req.quiz.destroy().then(function(){
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
+
+// Este soy yo!!!
 exports.author = function(req, res){
     res.render('author', { nombre: 'Abel Castosa' });
 }
--- models/quiz.js
+++ models/quiz.js
@@ -2,7 +2,17 @@
 
 module.exports = function(sequelize, DataTypes){
   return sequelize.define('Quiz',{
-    pregunta: DataTypes.STRING,
-    respuesta: DataTypes.STRING,
+    pregunta: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: "-> Falta pregunta"}}
+    },
+    respuesta: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: "-> Falta respuesta"}}
+    },
+    categoria: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: "-> Falta categoria"}}
+    },
   });
 }
